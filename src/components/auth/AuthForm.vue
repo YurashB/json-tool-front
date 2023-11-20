456ec1a5e2970be1ae4eed31005876eb4ecc758e
Bohdan Yurash <yurash.bogdan@gmail.com>
2023-11-20 12:10:42 +0200

    some test changes

---
+++ src/components/auth/AuthForm.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    <form class="space-y-6" action="#" method="POST" novalidate>
+      <div>
+        <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+        <div class="mt-2">
+          <input
+              id="email"
+              name="email"
+              type="email"
+              required=""
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+        </div>
+        <span
+            class="mt-2 text-sm text-red-500">
+            Please enter a valid email address
+          </span>
+      </div>
+
+      <div>
+        <div class="flex items-center justify-between">
+          <label for="password" class="block text-sm font-medium leading-6 text-gray-900">Password</label>
+        </div>
+        <div class="mt-2">
+          <input
+              id="password"
+              name="password"
+              type="password"
+              required=""
+              placeholder=" "
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+        </div>
+        <span
+            class="mt-2 text-sm text-red-500">
+            Please enter a valid password address
+          </span>
+      </div>
+</template>
+
+<style scoped>
+
+</style>
